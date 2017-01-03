ded6b4f872a3867ff8567c5efd219b2a0f8fa471
Paul Pham <paul@invisible.college>
2017-01-02 20:05:10 -0500

    Use new soundgram classes in SoundRecyclerAdapter, but updating buttons should be in onBindViewHolder.

--- app/src/main/java/college/invisible/glx/MainActivity.java
+++ app/src/main/java/college/invisible/glx/MainActivity.java
@@ -4,10 +4,12 @@ import android.content.Context;
 import android.media.MediaRecorder;
 import android.media.MediaPlayer;
 import android.os.Environment;
+import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,182 +17,69 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import college.invisible.soundgram.*;
 
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String LOG_TAG = "AudioRecordTest";
-    private static String mFileName = null;
-
-    private RecordButton mRecordButton = null;
-    private MediaRecorder mRecorder = null;
-
-    private PlayButton   mPlayButton = null;
-    private MediaPlayer   mPlayer = null;
-
-    private RecyclerView.Adapter mAdapter;
-
-    private void onRecord(boolean start) {
-        if (start) {
-            startRecording();
-        } else {
-            stopRecording();
-        }
-    }
-
-    private void onPlay(boolean start) {
-        if (start) {
-            startPlaying();
-        } else {
-            stopPlaying();
-        }
-    }
-
-    private void startPlaying() {
-        mPlayer = new MediaPlayer();
-        try {
-            mPlayer.setDataSource(mFileName);
-            mPlayer.prepare();
-            mPlayer.start();
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "prepare() failed");
-        }
-    }
-
-    private void stopPlaying() {
-        mPlayer.release();
-        mPlayer = null;
-    }
-
-    private void startRecording() {
-        mRecorder = new MediaRecorder();
-        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        mRecorder.setOutputFile(mFileName);
-        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-
-        try {
-            mRecorder.prepare();
-            mRecorder.start();
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "prepare() failed");
-        }
-
-
-    }
-
-    private void stopRecording() {
-        mRecorder.stop();
-        mRecorder.release();
-        mRecorder = null;
-    }
-
-    class RecordButton extends Button {
-        boolean mStartRecording = true;
-
-        public RecordButton(Context ctx) {
-            super(ctx);
-            setText("Start recording");
-            setOnClickListener(new OnClickListener() {
-                public void onClick(View v) {
-                    onRecord(mStartRecording);
-                    if (mStartRecording) {
-                        setText("Stop recording");
-                    } else {
-                        setText("Start recording");
-                    }
-                    mStartRecording = !mStartRecording;
-                }
-            });
-        }
-    }
-
-    class PlayButton extends Button {
-        boolean mStartPlaying = true;
-
-        public PlayButton(Context ctx) {
-            super(ctx);
-            setText("Start playing");
-            setOnClickListener(new OnClickListener() {
-                public void onClick(View v) {
-                    onPlay(mStartPlaying);
-                    if (mStartPlaying) {
-                        setText("Stop playing");
-                    } else {
-                        setText("Start playing");
-                    }
-                    mStartPlaying = !mStartPlaying;
-                }
-            });
-        }
-    }
+    private RecyclerView mRecyclerView;
 
     public MainActivity() {
-        mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
-        mFileName += "/audiorecordtest.3gp";
-        Log.i(LOG_TAG, "Filename " + mFileName);
+        SoundGramControls.checkAndCreateDirectory();
     }
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
-        /*
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        */
-        //setSupportActionBar(toolbar);
+
+        setSupportActionBar(toolbar);
 
         // Programmatically set the layout
         LinearLayout ll = new LinearLayout(this);
-        mRecordButton = new RecordButton(this);
-        ll.addView(mRecordButton,
-                new LinearLayout.LayoutParams(
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        0));
-        mPlayButton = new PlayButton(this);
-        ll.addView(mPlayButton,
-                new LinearLayout.LayoutParams(
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        0));
+
+        SoundGramControls sgc = new SoundGramControls(ll, this.getApplicationContext(), "First Gram");
 
 
         LayoutInflater inflater =
                 (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        RecyclerView mRecyclerView = new RecyclerView(getApplicationContext());
+        //RecyclerView mRecyclerView = new RecyclerView(getApplicationContext());
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        //mRecyclerView.setHasFixedSize(true);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
-        inflater.inflate(R.layout.content_sound, mRecyclerView, true);
+        SoundRecyclerAdapter mAdapter = new SoundRecyclerAdapter(mRecyclerView);
+        mRecyclerView.setAdapter(mAdapter);
 
-        /*
+/*
         ll.addView(mRecyclerView,
                 new RecyclerView.LayoutParams(
                         ViewGroup.LayoutParams.WRAP_CONTENT,
                         ViewGroup.LayoutParams.WRAP_CONTENT
                 ));
-                */
+*/
         // specify an adapter (see also next example)
 
-        //mAdapter = new SoundRecyclerAdapter(mRecyclerView);
-        //mRecyclerView.setAdapter(mAdapter);
-
-        setContentView(ll);
+        mAdapter.addItem(0, "ba ba black sheep");
+        mAdapter.addItem(1, "three bags full");
+        //inflater.inflate(R.layout.content_sound, mRecyclerView, true);
+        /*
+        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.layout.app_bar_layout);
+        appBarLayout.addView(ll,
+                new AppBarLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT
+                ));
+                */
+        //setContentView(ll);
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        if (mRecorder != null) {
-            mRecorder.release();
-            mRecorder = null;
-        }
-
-        if (mPlayer != null) {
-            mPlayer.release();
-            mPlayer = null;
-        }
     }
 }
--- app/src/main/java/college/invisible/glx/SoundRecyclerAdapter.java
+++ app/src/main/java/college/invisible/glx/SoundRecyclerAdapter.java
@@ -5,11 +5,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import college.invisible.soundgram.SoundGramSample;
+import college.invisible.soundgram.SoundGram;
 
 /**
  * Created by ppham on 2/23/16.
@@ -59,9 +64,7 @@ public class SoundRecyclerAdapter extends RecyclerView.Adapter<SoundRecyclerAdap
     }
 
     public void addItem(int positionToAdd, String soundGramTitle) {
-        String newName = positionToAdd + "";
-        SoundGram sgram = new SoundGram(newName);
-        sgram.setName(soundGramTitle);
+        SoundGram sgram = new SoundGram(soundGramTitle);
         sampleData.add(positionToAdd, sgram);
         notifyItemInserted(positionToAdd);
     }
@@ -74,21 +77,62 @@ public class SoundRecyclerAdapter extends RecyclerView.Adapter<SoundRecyclerAdap
             View.OnClickListener {
 
         private final TextView nameView;
+        private final Button recordButton; // there is a single record button for each sound gram
+        private final List<Button> playButtons; // there is a playbutton for each sample
 
+        /*
+         * The constructor is where we initialize this ViewHolder and its sub-views.
+         */
         public ViewHolder(View itemView) {
             super(itemView);
 
             nameView = (TextView) itemView.findViewById(
                     R.id.name_view);
+            recordButton = (Button) itemView.findViewById(R.id.record_button);
+            playButtons = new ArrayList<>();
             itemView.setOnClickListener(this);
         }
 
+        /*
+         * The onClick method is where we rebind soundgram data to a view when it becomes visible.
+         * This involves updating the state of static members (whose numbers do not change between
+         * data items), and removing / updating
+         * dynamic items (whose numbers could change from data to data item).
+         * The view is passed in by RecyclerAdapter base class.
+         */
         @Override
         public void onClick(View v) {
+            LinearLayout ll = (LinearLayout) v;
             // v is the LinearLayout, it turns out, the parent.
             int position = this.getLayoutPosition();
             SoundGram data = (SoundGram) this.itemView.getTag();
             nameView.setText(data.getName());
+            List<SoundGramSample> samples = data.getSamples();
+            if (samples.size() > playButtons.size()) {
+                // Add more buttons to accommodate more samples than current buttons
+                int diff = samples.size() - playButtons.size();
+                for (int i = 0; i < diff; i++) {
+                    // create buttons with same context as the single record button
+                    Button newButton = new Button(recordButton.getContext());
+                    playButtons.add(newButton);
+                    ll.addView(newButton);
+                }
+            } else if (samples.size() < playButtons.size()) {
+                int diff = playButtons.size() - samples.size();
+                for (int i = 0; i< diff; i++) {
+                    Button oldButton = playButtons.get(0);
+                    ll.removeView(oldButton);
+                    playButtons.remove(oldButton); // remove all at the beginning
+                }
+            } // else they are equal, and we don't add/remove anything.
+            assert(playButtons.size() == samples.size());
+
+            for (int i = 0; i < samples.size(); i++) {
+                Button playButton = playButtons.get(i);
+                SoundGramSample sample = samples.get(i);
+                playButton.setText(sample.getSpeakerName());
+                sample.setOnClickListener(playButton);
+            }
             Log.d("Tag1", "At position + " + Integer.toString(position) + " " + v.toString());
         }
     }
